328505d66ac068cb61cd39b8e06a15f90c02436d
Zhonghua Yan <ccnuyan@live.com>
2017-08-29 15:41:36 +0800

    ready to publish

--- webpack.build-config.js
+++ webpack.build-config.js
@@ -15,7 +15,7 @@ const config = {
   output: {
     filename: '[name]-[hash].js',
     path: path.join(__dirname, './build/assets/'),
-    publicPath: './assets/',
+    publicPath: './static/assets/',
   },
   plugins: [
     new webpack.DefinePlugin({
